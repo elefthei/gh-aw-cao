aadf6896ed1f3db935268dae8bc49d2a1677dcca
Mara Nikola Kiefer <8320933+mnkiefer@users.noreply.github.com>
2026-09-15 20:14:27 +0200

    Update terminology for AWs in dashboard and docs (#11727)

--- dashboard/site/test/unit/presenter.test.js
+++ dashboard/site/test/unit/presenter.test.js
@@ -2690,7 +2690,7 @@ describe('presenter built-in and custom pages', () => {
             { field: 'runs', type: 'quantitative', title: 'Runs' },
             { field: 'failure-summary', type: 'nominal', title: 'Failure rate', filter: false },
             { field: 'aic', type: 'quantitative', title: 'Local AIC', unit: 'aic' },
-            { field: 'workflows', type: 'quantitative', title: 'Local AWs' },
+            { field: 'workflows', type: 'quantitative', title: 'AWs' },
             { field: 'status', type: 'nominal', title: 'Status', display: 'status' }
           ],
           href: { field: 'repository-link', type: 'nominal' }
